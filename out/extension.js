a3db5ec6ddd50f86cdb1e917d93ce7a071a765ef
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-11-02 18:05:27 +0000

    first commit

---
+++ out/extension.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+function activate(context) {
+    //   vscode.workspace.getConfiguration().update("workbench.iconTheme", "moonwallIcons", true);
+}
+exports.activate = activate;
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
---
+++ src/extension.ts
@@ -0,0 +1,7 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+//   vscode.workspace.getConfiguration().update("workbench.iconTheme", "moonwallIcons", true);
+}
+
+export function deactivate() {}
